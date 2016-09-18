4a648cfe26c4114afbc50449ff58c25bf1c9a9ff
AustinB18 <apb16@students.uwf.edu>
2016-09-18 15:24:33 -0500

    Delete tokens.py

--- tokens.py
+++
@@ -1,6 +0,0 @@
-import os
-
-app_key = 'QAtMo1K7on2AyQ'
-app_secret = 'KtJbX8pTkQbLJnh_EFrjd_ucZRU'
-refresh_tocken = '41325460-KqGOwCNZ8xjD-lK0J2baXrrXxNc'
-access_token = 'A-NZ5iiYhD1JnMZnJcjo2F5WPLI'
